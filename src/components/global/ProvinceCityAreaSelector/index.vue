522a64afc46bf154b2825826ef90750f194d4276
leibb <czleing@163.com>
2025-01-14 18:52:53 +0800

    修复一些问题

--- src/components/global/ProvinceCityAreaSelector/index.vue
+++ src/components/global/ProvinceCityAreaSelector/index.vue
@@ -1,4 +1,4 @@
-<!-- 省、省市、省市区选择器，绑定值为所选最后一级编码逗号分隔字符串 -->
+<!-- 省、省市、省市区选择器，单选、多选，绑定值为所选最后一级编码逗号分隔字符串，如：'110101' 或 '110101,110102' -->
 <!-- 单选 -->
 <!-- 省市区用法：<ProvinceCityArea v-model:value="area" /> -->
 <!-- 省市用法：<ProvinceCityArea v-model:value="area" :levelNumber="2" /> -->
@@ -52,6 +52,10 @@ const selectedNames = computed(() => {
     return '-'
   }
 })
+// 直辖市、特别行政区 Codes，这种 code ，依赖数据的省级下面直接是区，没有市，需要特殊处理
+const specialCodes = ['110000', '120000', '310000', '500000', '810000', '820000']
+// 还原层级时需要排除的 codes
+const excludeCodes = specialCodes.map(code => code.substring(0, 2) + '0100')
 
 onMounted(() => {
   initData()
@@ -66,17 +70,21 @@ watch(
 )
 
 function setValue (val) {
-  // 将只有最后一层还原成 ant-design 组件需要的三层格式
-  // 1011 => [1000, 1011]
-  // 101102 => [100000,101100,101102]
-  // 101102,101103 => [[100000,101100,101102], [100000,101100,101103]]
+  // 将只有最后一层还原成 ant-design 组件需要的三层格式，此处特殊的直辖市、经济特区只有两层，需排除中间一层
+  // '1011' => [1000, 1011] // 选择的是第二级，还原时只需还原两级
+  // '110000' => [110000] // 选择的是第一级，还原时只还原一级
+  // '101102' => [100000,101100,101102] // 选择的是第三级，还原时需还原三级
+  // '101102,221103' => [[100000,101100,101102], [220000,221100,221103]] // 多选时，选择多个第三级，还原时每个还原出三级
   const values = val ? val.split(',') : []
   selectedValue.value = values.map(leafCode => {
     const level = leafCode.length / 2
     const codes = []
     for (let i = 0; i < level; i++) {
       if (leafCode.substring(i * 2, i * 2 + 2) !== '00') {
-        codes.push(leafCode.substring(0, i * 2 + 2).padEnd(leafCode.length, '0'))
+        const code = leafCode.substring(0, i * 2 + 2).padEnd(leafCode.length, '0')
+        if (!excludeCodes.includes(code.padEnd(6, '0'))) {
+          codes.push(code)
+        }
       }
     }
     return codes
@@ -87,21 +95,25 @@ function initData () {
   // console.log('===province==', province)
   // console.log('===city==', city)
   // console.log('===area==', area)
+
+  const getArea = (province, city) => {
+    return props.levelNum >= 3 ? area.filter(a => a.province === province && a.city === city).map(a => ({
+      value: a.code,
+      label: a.name
+    })) : undefined
+  }
+
+  const getCity = (province) => {
+    return props.levelNum >= 2 ? city.filter(c => c.province === province).map(c => ({
+      value: c.code,
+      label: c.name,
+      children: getArea(c.province, c.city)
+    })) : undefined
+  }
   options.value = province.map(p => ({
     value: p.code,
     label: p.name,
-    children: props.levelNum >= 2
-      ? city.filter(c => c.province === p.province).map(c => ({
-          value: c.code,
-          label: c.name,
-          children: props.levelNum >= 3
-            ? area.filter(a => a.province === c.province && a.city === c.city).map(a => ({
-                value: a.code,
-                label: a.name
-              }))
-            : undefined
-        }))
-      : undefined
+    children: specialCodes.includes(p.code) ? getArea(p.province, '01') : getCity(p.province)
   }))
 }
 
@@ -109,15 +121,14 @@ const emits = defineEmits(['update:value'])
 
 function handleChange (val) {
   selectedValue.value = val
-  // 只返回最后一层，并且以字符串形式
+  // 只返回最后一层，并且以字符串形式逗号拼接
   // 非三级单选时：[1000,1010] => '1010'
   // 单选时：[100000,101000,101010] => '101010'
   // 多选时：[[100000,101000,101010], [100000,101000,101011]] => '101010,101011'
-  function isArrArr (val) {
-    return val && Array.isArray(val) && val.length && Array.isArray(val[0])
+  function isArrArr (value) {
+    return value && Array.isArray(value) && value.length && Array.isArray(value[0])
   }
-  const returnValue = isArrArr(val) ? val.map(arr => arr.at(-1)).join(',') : val.at(-1)
-  // console.log('returnValue', returnValue)
+  const returnValue = isArrArr(val) ? val.map(arr => arr.at(-1)).join(',') : val?.at(-1)
   emits('update:value', returnValue)
 }
 
