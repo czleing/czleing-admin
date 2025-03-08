8189122391d8c94fef925897d0fc5ac0929f0724
czleing@163.com <czleing@163.com>
2025-03-08 21:39:45 +0800

    ✨ feat: 导入功能扩展

--- src/components/crud/components/c-import.vue
+++ src/components/crud/components/c-import.vue
@@ -1,8 +1,10 @@
 <!-- 导入按钮 -->
 <template>
   <div class="c-import">
-    <a-button type="dashed" :icon="h(ImportOutlined)" @click="onImportHandle">{{ btnText }}</a-button>
-    <Modal ref="cModal" :title="title" width="450" :confirm-disabled="uploadDisabled" @close="beforeCloseHandle">
+    <slot>
+      <a-button v-if="showBtn" type="dashed" :icon="h(ImportOutlined)" @click="onImportHandle">{{ btnText }}</a-button>
+    </slot>
+    <Modal ref="cModal" width="450" :confirm-disabled="uploadDisabled" @close="beforeCloseHandle">
       <div class="mb">
         点击此处下载
         <a href="javascript:;" @click="downloadTemplate">导入模板</a>
@@ -24,7 +26,7 @@
           </p>
         </div>
       </a-upload-dragger>
-      <div class="mt">
+      <div v-if="showReplace" class="mt">
         重复记录是否覆盖更新：<a-switch v-model:checked="isReplace" />
       </div>
     </Modal>
@@ -39,25 +41,17 @@ import { loadingRequest } from '@/utils/index.js'
 
 const props = defineProps({
   // 上传地址
-  url: {
-    type: String,
-    required: true
-  },
+  url: { type: String },
   // 模板下载地址
-  templateUrl: {
-    type: String,
-    required: true
-  },
+  templateUrl: { type: String },
   // 弹出层标题
-  title: {
-    type: String,
-    default: '导入'
-  },
+  title: { type: String, default: '导入' },
+  // 是否显示覆盖更新控制按钮
+  showReplace: { type: Boolean, default: true },
+  // 是否显示导入按钮
+  showBtn: { type: Boolean, default: true },
   // 导入按钮文字
-  btnText: {
-    type: String,
-    default: '导入'
-  }
+  btnText: { type: String, default: '导入' }
 })
 
 const cModal = ref()
@@ -66,13 +60,34 @@ const fileList = ref([])
 const uploading = ref(false)
 const uploadDisabled = computed(() => fileList.value.length === 0 || uploading.value)
 
+const currUrl = ref(props.url)
+const currTemplateUrl = ref(props.templateUrl)
+const currTitle = ref(props.title)
+
+/** 通过 API 方式调用弹出导入窗，并设置相关属性 */
+function open (option = {}) {
+  currUrl.value = option.url
+  currTemplateUrl.value = option.templateUrl
+  currTitle.value = option.title
+  onImportHandle()
+}
+
 function onImportHandle () {
+  if (!currUrl.value) {
+    console.error('未配置导入接口[url]')
+    return
+  }
   cModal.value.open({
+    title: currTitle.value,
     onConfirm: upload
   })
 }
 function downloadTemplate () {
-  axios.download(props.templateUrl)
+  if (!currTemplateUrl.value) {
+    console.error('未配置模板下载接口[templateUrl]')
+    return
+  }
+  axios.download(currTemplateUrl.value)
 }
 function handleRemove () {
   fileList.value = []
@@ -85,7 +100,7 @@ function upload (close) {
   const formData = new FormData()
   formData.append('file', fileList.value[0])
   loadingRequest(uploading, async () => {
-    await axios.post(props.url + `?updateSupport=${isReplace.value}`, formData)
+    await axios.post(currUrl.value + `?updateSupport=${isReplace.value}`, formData)
     beforeCloseHandle()
     close()
     emits('success')
@@ -97,6 +112,10 @@ function beforeCloseHandle () {
 }
 
 const emits = defineEmits(['success'])
+
+defineExpose({
+  open
+})
 </script>
 
 <style lang="scss" scoped>
