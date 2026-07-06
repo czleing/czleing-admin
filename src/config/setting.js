f2e879e762d5761bf6b0a76f984014ddcc932cb9
leibingbing <czleing@163.com>
2026-07-06 17:41:04 +0800

    chore: 修改网站信息配置

--- src/config/setting.js
+++ src/config/setting.js
@@ -2,10 +2,10 @@ export default {
   // 网站信息
   websiteInfo: {
     systemName: 'czleing-admin', // 网站名称
-    systemDesc: 'Vue3.4 + vite4.5 + ant-design-vue4.2 + pinia2.1', // 网站描述
+    systemDesc: 'Vue3.5 + vite8 + ant-design-vue4.2 + pinia3', // 网站描述
     logo: './logo.png', // new URL('@/assets/images/logo.png', import.meta.url), // 网站 LOGO
     logoWhite: './logo_white.png', // new URL('@/assets/images/logo_white.png', import.meta.url), // 网站白色 LOGO
     copyright: '粤网安备：888888888', // 备案号，版权号
-    copyrightLink: 'https://gitee.com/czleing/base-backend-static' // 备案号链接
+    copyrightLink: 'https://gitee.com/czleing/czleing-admin' // 备案号链接
   }
 }
