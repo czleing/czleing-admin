b75014535eed516c0c111042fa643dcf3e878568
czleing@163.com <czleing@163.com>
2026-07-18 00:09:11 +0800

    style: 删除日志

--- src/stores/tabs-store.js
+++ src/stores/tabs-store.js
@@ -104,7 +104,6 @@ export const useTabsStore = defineStore('tabs', {
         return []
       }
       const currTab = this.tabList[this.currentIndex]
-      console.log('currTab', currTab)
       const currComName = currTab?.matched?.at(-1)?.componentName
       const cachedViews = this.tabList.map(tab => {
         const componentName = tab.matched.at(-1)?.componentName
